2df768247bb4792a5f12cc35174cb1f93ec16236
Geovanna Vitória <geovannavitmlopes@gmail.com>
2026-01-06 12:17:01 -0300

    adicionando outros components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Menu } from './components/menu/menu';
+import { Header } from './components/header/header';
+import { Projects } from './components/projects/projects';
+import { AboutMe } from './components/about-me/about-me';
+import { Servicos } from './components/servicos/servicos';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    Menu,
+    Header,
+    Projects,
+    AboutMe,
+    Servicos
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
